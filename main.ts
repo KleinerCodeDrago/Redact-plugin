d4f24c92d8420aff2391d1e5f02cf427c38381e2
AugenAugen <allthestuffilove@protonmail.com>
2024-04-22 09:41:33 +0200

    path problem

--- main.ts
+++ main.ts
@@ -1,134 +1,111 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { normalize } from 'path';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface RedactorPluginSettings {
+  redactedVaultPath: string;
+  redactionSymbol: string;
+  redactionMarker: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: RedactorPluginSettings = {
+  redactedVaultPath: '',
+  redactionSymbol: '█',
+  redactionMarker: '==',
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class RedactorPlugin extends Plugin {
+  settings: RedactorPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    this.addCommand({
+      id: 'redact-and-extract',
+      name: 'Redact and Extract',
+      editorCallback: async (editor: Editor, view: MarkdownView) => {
+        const fileContent = editor.getValue();
+        const redactedVaultPath = normalize(this.settings.redactedVaultPath);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        if (redactedVaultPath) {
+          const redactedFileContent = fileContent.replace(
+            new RegExp(`${this.settings.redactionMarker}(.*?)${this.settings.redactionMarker}`, 'gs'),
+            (_, text) => this.settings.redactionSymbol.repeat(text.length)
+          );
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+          const redactedFilePath = `${redactedVaultPath}/${view.file.name}`;
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+          try {
+            await this.app.vault.adapter.write(redactedFilePath, redactedFileContent);
+            new Notice(`Redacted text extracted to ${redactedFilePath}`);
+          } catch (error) {
+            console.error('Error extracting redacted text:', error);
+            new Notice('An error occurred while extracting the redacted text. Please check the console for more details.');
+          }
+        } else {
+          new Notice('Please set the redacted vault path in the plugin settings.');
+        }
+      }
+    });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    this.addSettingTab(new RedactorSettingTab(this.app, this));
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  onunload() {
+  }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class RedactorSettingTab extends PluginSettingTab {
+  plugin: RedactorPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(app: App, plugin: RedactorPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+  display(): void {
+    const {containerEl} = this;
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+    containerEl.empty();
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    new Setting(containerEl)
+      .setName('Redacted Vault Path')
+      .setDesc('Absolute path to the vault where redacted text will be extracted')
+      .addText(text => text
+        .setPlaceholder('Enter the redacted vault path')
+        .setValue(this.plugin.settings.redactedVaultPath)
+        .onChange(async (value) => {
+          this.plugin.settings.redactedVaultPath = value;
+          await this.plugin.saveSettings();
+        }));
 
-	display(): void {
-		const {containerEl} = this;
+    new Setting(containerEl)
+      .setName('Redaction Symbol')
+      .setDesc('Symbol used to replace redacted text')
+      .addText(text => text
+        .setPlaceholder('Enter the redaction symbol')
+        .setValue(this.plugin.settings.redactionSymbol)
+        .onChange(async (value) => {
+          this.plugin.settings.redactionSymbol = value;
+          await this.plugin.saveSettings();
+        }));
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Redaction Marker')
+      .setDesc('Marker used to indicate the start and end of redacted text')
+      .addText(text => text
+        .setPlaceholder('Enter the redaction marker')
+        .setValue(this.plugin.settings.redactionMarker)
+        .onChange(async (value) => {
+          this.plugin.settings.redactionMarker = value;
+          await this.plugin.saveSettings();
+        }));
+  }
 }
